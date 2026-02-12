6b9ecdca6ed8ce61428ef9b59cf804960ad920d1
Tom <tbvanreijn@gmail.com>
2026-02-12 21:21:22 +0100

    pay

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -65,6 +65,7 @@ export async function POST(req: Request) {
         },
       ],
       mode: "payment",
+      allow_promotion_codes: true,
       success_url: `${baseUrl}/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${baseUrl}/inschrijven/${courseId}`,
       automatic_tax: { enabled: false },
